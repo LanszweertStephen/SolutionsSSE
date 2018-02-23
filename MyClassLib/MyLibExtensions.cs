e1cb42bd582950d1c23c7eaaa5b4c8fb5922e575
LanszweertStephen <stephen.lanszweert@student.howest.be>
2018-02-23 13:27:52 +0100

    Last oef of week2 still todo

---
+++ MyClassLib/MyLibExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClassLib
+{
+    public static class MyLibExtensions
+    {
+        public static bool IsOdd(this int i)
+        {
+            return i % 2 == 0;
+        }
+        public static int CountChar(this string str,char theLetter)
+        {
+            int count = 0;
+            string[] parts = str.Split(" ");
+            foreach(string s in parts){
+                char[] let = s.ToLower().ToCharArray();
+                foreach(char st in let)
+                {
+                    if (st.Equals(theLetter))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+}
--- SolutionsWeek2/Program.cs
+++ SolutionsWeek2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyClassLib;
+using System;
 
 namespace SolutionsWeek2
 {
@@ -6,7 +7,20 @@ namespace SolutionsWeek2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Oefening 1
+            int getal = 2;
+            if (getal.IsOdd())
+            {
+                Console.WriteLine("Number is even");
+            } else
+            {
+                Console.WriteLine("Number is not even");
+            }
+
+            // Oefening 2
+            string toCheck = "Hello world hehe";
+            char letterToCheck = 'h';
+            Console.WriteLine($"'{toCheck}' : found '{letterToCheck}' {toCheck.CountChar(letterToCheck)} time(s).");
         }
     }
 }
